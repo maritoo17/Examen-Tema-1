05fc780e237182049a85748c125c2de01ad83c39
maritoo17 <msancgim@myuax.com>
2023-11-09 11:46:55 +0100

    Primer ejercicio de la parte dos en la que nos pide que hagamos una suma entre dos números.

---
+++ Ejercicio 1/suma.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a, b;
+    cout << "Ingrese el primer numero: ";
+    cin >> a;
+    cout << "Ingrese el segundo numero: ";
+    cin >> b;
+    cout << "La suma de los numeros es: " << a + b << endl;
+    return 0;
+}
