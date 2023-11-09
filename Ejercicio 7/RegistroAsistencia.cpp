6d54462753c7ceef0ff178f2854a1aa929750722
maritoo17 <msancgim@myuax.com>
2023-11-09 13:01:31 +0100

    Arreglo y finalización del ejercicio 7.

--- Ejercicio 7/RegistroAsistencia.cpp
+++ Ejercicio 7/RegistroAsistencia.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 class RegistroAsistencia {
+public:
     string fecha;
     string estado;
 
@@ -17,9 +18,21 @@ int main() {
     RegistroAsistencia registro1;
 
     registro1.fecha = "2021-09-01";
-    registro1.estado = "Presente";
+    registro1.estado = "Asistió";
+
+    RegistroAsistencia registro2;
+
+    registro2.fecha = "2021-09-01";
+    registro2.estado = "Falta";
+
+    RegistroAsistencia registro3;
+
+    registro3.fecha = "2021-09-01";
+    registro3.estado = "Tardanza";
 
     registro1.mostrar_asistencia();
+    registro2.mostrar_asistencia();
+    registro3.mostrar_asistencia();
 
     return 0;
 }
